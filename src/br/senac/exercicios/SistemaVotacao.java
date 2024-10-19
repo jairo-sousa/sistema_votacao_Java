2c5f3be7ea912f95cd4ac2730e42ec696f182675
jairo-sousa <jairo.sousa.rufino@gmail.com>
2024-10-19 14:34:15 -0300

    Executar audio em thread separada

--- src/br/senac/exercicios/SistemaVotacao.java
+++ src/br/senac/exercicios/SistemaVotacao.java
@@ -5,23 +5,27 @@ import javax.sound.sampled.*;
 
 public class SistemaVotacao {
     public static void tocarSomConfirmacao(){
-        try {
-            URL audioUrl = new URL("https://raw.githubusercontent.com/jairo-sousa/sistema_votacao_Java/refs/heads/main/audio/confirma.wav");
-            Clip oClip = AudioSystem.getClip();
-            AudioInputStream oStream = AudioSystem.getAudioInputStream(audioUrl);
-
-            oClip.open(oStream);
-            oClip.start();
-
-            long duration = oClip.getMicrosecondLength();
-            Thread.sleep(duration / 1000);
-
-            oClip.close();
-            oStream.close();
-        
-        } catch (Exception e) {
-            e.printStackTrace();
+        new Thread (() -> {
+            try {
+                URL audioUrl = new URL("https://raw.githubusercontent.com/jairo-sousa/sistema_votacao_Java/refs/heads/main/audio/confirma.wav");
+                Clip oClip = AudioSystem.getClip();
+                AudioInputStream oStream = AudioSystem.getAudioInputStream(audioUrl);
+    
+                oClip.open(oStream);
+                oClip.start();
+    
+                long duration = oClip.getMicrosecondLength();
+                Thread.sleep(duration / 1000);
+    
+                oClip.close();
+                oStream.close();
+            
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
+
+        ).start();
     }
     public static void main(String[] args) {
         tocarSomConfirmacao();
